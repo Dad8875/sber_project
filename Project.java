75f1e658ab3b33ca8edf37821d31287ad88fa107
Dad8875 <Serstep1956@yahoo.com>
2022-09-19 23:48:08 +0300

    change file

--- Project.java
+++ Project.java
@@ -2,37 +2,53 @@ import java.util.Scanner;
 
 public class Project {
   public static void main(String[] args) {
-        final double ROUBLES_PER_USD = 72.12;
+      final double ROUBLES_PER_USD = 72.12;
 
-        int dollars;
-        double roubles;
-        int digit;
+      int dollars;
+      double roubles;
+      int digit; // последняя цифра dollars
+      int n; // количество конвертаций
+      int i; // счетчик
 
-        Scanner scanner = new Scanner(System.in);
+      Scanner scanner = new Scanner(System.in);
 
-        // Получить сумму денег в usd.
-        System.out.print("Введите сумму денег в долларах: ");
-        dollars = scanner.nextInt();
+      // получать количество конвертаций , до тех пор,
+      // пока не введено верное значение
+      do {
+          System.out.print("Введите корректное количество конвертаций: ");
+          n = scanner.nextInt();
+      } while (n <= 0);
+      /*
+      До тех пор , пока не конвертированы все суммы,
+      получать , отображать и конвертировать суммы денег в американских долларах
+      и отображать суммы денег в российских рублях
+       */
+      for (int j = 0; j < n; j++) {
 
-        //отобразить сумму в долларах с правильным окончанием
-        System.out.print(dollars);
-        if (5 <= dollars && dollars <= 20) {
-            System.out.print(" американских долларов равны");
-        } else {
-            digit = dollars % 10;
+          // Получить сумму денег в usd.
+          System.out.print("Введите сумму денег в долларах: ");
+          dollars = scanner.nextInt();
 
-            if (digit == 1)
-                System.out.print(" американский доллар равен ");
-            else if (2 <= digit && digit <= 4)
-                System.out.print(" американских доллара равны ");
-            else
-                System.out.print(" американсикй долларов равны ");
-        }
+          //отобразить сумму в долларах с правильным окончанием
+          System.out.print(dollars);
+          if (5 <= dollars && dollars <= 20) {
+              System.out.print(" американских долларов равны");
+          } else {
+              digit = dollars % 10;
 
-        //Конвертировать сумму денег в российские рубли.
-        roubles = dollars * ROUBLES_PER_USD;
+              if (digit == 1)
+                  System.out.print(" американский доллар равен ");
+              else if (2 <= digit && digit <= 4)
+                  System.out.print(" американских доллара равны ");
+              else
+                  System.out.print(" американсикй долларов равны ");
+          }
 
-        //Отобразить сумму денег в российских рублях в пользу покупателя.
-        System.out.println((int)(roubles * 100) / 100.0 + " российских рублей");
+          //Конвертировать сумму денег в российские рубли.
+          roubles = dollars * ROUBLES_PER_USD;
+
+          //Отобразить сумму денег в российских рублях в пользу покупателя.
+          System.out.println((int) (roubles * 100) / 100.0 + " российских рублей");
+    }
   }
 }
